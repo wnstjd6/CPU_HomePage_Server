68c0633277b9b5157d9f46c6c9b4c4b9a4ea80ef
wnstjd6 <s2406@e-mirim.hs.kr>
2026-03-02 03:43:11 +0900

    feat: 테스트 화면

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationModule } from './application/application.module';
 import { QuestionModule } from './question/question.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AppController } from './app.controller';
 
 @Module({
+  controllers: [AppController],
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     ApplicationModule,
